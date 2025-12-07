fad459c480c7006ef31907f5a642f071c0b1edc7
Ana Ruth <aniarinna@gmail.com>
2025-12-07 12:17:40 +0100

    feat: add Month model class for JavaCollections exercise

---
+++ src/sprint01/JavaCollections/level01A/Month.java
@@ -0,0 +1,39 @@
+package sprint01.JavaCollections.level01A;
+
+import java.util.Objects;
+
+public class Month {
+
+    private final String name;
+
+    public Month(String name) {
+        this.name = Objects.requireNonNull(name,  "name cannot be null" );
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Month)) return false;
+
+        Month other = (Month) o;
+
+        return name.equalsIgnoreCase(other.name);
+    }
+
+        @Override
+                public int hashCode() {
+            return name.toLowerCase().hashCode();
+
+
+    }
+}
---
+++ src/sprint01/JavaCollections/level01A/MonthsMain.java
@@ -0,0 +1,4 @@
+package sprint01.JavaCollections.level01A;
+
+public class MonthsMain {
+}
